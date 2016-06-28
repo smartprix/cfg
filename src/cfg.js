a9a852ea9e2880412f8138d24fd61b5db4b7dc81
Hitesh Khandelwal <hitesh@smartprix.com>
2016-06-29 04:04:58 +0530

    sm-utils version 2.0.0

--- src/cfg.js
+++ src/cfg.js
@@ -1,27 +1,35 @@
-var _ = require('lodash');
+const _ = require('lodash');
 
-var config = {};
+let config = {};
 
-function cfg(key, default_value) {
-    if(key in config)
-        return config[key];
+function cfg(key, defaultValue) {
+	if (key in config) {
+		return config[key];
+	}
 
-    return default_value;
+	return defaultValue;
 }
 
-cfg.get = function() {
-    return cfg(key, default_value);
-}
+cfg.get = function (key, defaultValue) {
+	return cfg(key, defaultValue);
+};
 
-cfg.set = function(key, value) {
-    if(value === undefined && key instanceof Object) {
-        config = _.assign(config, key);
-        return null;
-    }
+// set values in global config
+// you can also give key as an object to assign all key values from it
+cfg.set = function (key, value) {
+	// if key is Object then merge it with existing config
+	if (value === undefined && key instanceof Object) {
+		config = _.assign(config, key);
+		return null;
+	}
 
-    var prev = config[key];
-    config[key] = value;
-    return prev;
-}
+	const prev = config[key];
+	config[key] = value;
+	return prev;
+};
+
+cfg.delete = function (key) {
+	delete config[key];
+};
 
 module.exports = cfg;
