1d623ff3c7379a31077b66f599eecea6aa26bad0
Hitesh Khandelwal <hitesh@smartprix.com>
2018-03-05 12:57:00 +0530

    read config files by default, and add memoize function to cache library

--- src/cfg.js
+++ src/cfg.js
@@ -1,13 +1,49 @@
 const _ = require('lodash');
 
-let config = {};
+const config = {};
+let configRead = false;
+
+// merge configs, takes care of getters while merging
+function merge(conf, confPrivate) {
+	if (!confPrivate) return conf;
+
+	Object.keys(confPrivate).forEach((key) => {
+		if (Object.prototype.hasOwnProperty.call(conf, key)) {
+			if (confPrivate[key] === Object(confPrivate[key])) {
+				if (conf[key] !== Object(conf[key])) conf[key] = {};
+				merge(conf[key], confPrivate[key]);
+			}
+			else {
+				Object.defineProperty(conf, key, Object.getOwnPropertyDescriptor(confPrivate, key));
+			}
+		}
+		else {
+			Object.defineProperty(conf, key, Object.getOwnPropertyDescriptor(confPrivate, key));
+		}
+	});
+
+	return conf;
+}
 
 function cfg(key, defaultValue) {
-	if (key in config) {
-		return config[key];
-	}
+	// eslint-disable-next-line no-use-before-define
+	readDefaultConfigFiles();
+	return _.get(config, key, defaultValue);
+}
 
-	return defaultValue;
+function readDefaultConfigFiles() {
+	if (configRead) return;
+	configRead = true;
+
+	const env = process.env.NODE_ENV || 'developement';
+	cfg.file(`${process.cwd()}/config.js`, {ignoreNotFound: true});
+	cfg.file(`${process.cwd()}/config.${env}.js`, {ignoreNotFound: true});
+	cfg.file(`${process.cwd()}/private/config.js`, {ignoreNotFound: true});
+	cfg.file(`${process.cwd()}/private/config.${env}.js`, {ignoreNotFound: true});
+
+	if (config.$privateConfigFile) {
+		cfg.file(config.$privateConfigFile, {ignoreNotFound: true});
+	}
 }
 
 cfg.get = function (key, defaultValue) {
@@ -17,14 +53,18 @@ cfg.get = function (key, defaultValue) {
 // set values in global config
 // you can also give key as an object to assign all key values from it
 cfg.set = function (key, value) {
+	readDefaultConfigFiles();
+
 	// if key is Object then merge it with existing config
 	if (value === undefined && key instanceof Object) {
-		config = _.assign(config, key);
+		const env = process.env.NODE_ENV || 'developement';
+		Object.assign(config, key);
+		Object.assign(config, key[`$env_${env}`]);
 		return null;
 	}
 
-	const prev = config[key];
-	config[key] = value;
+	const prev = _.get(config, key);
+	_.set(config, key, value);
 	return prev;
 };
 
@@ -32,15 +72,68 @@ cfg.set = function (key, value) {
 // if a key already exists then it is merged with new value
 // if obj is not an Object then nothing happens
 cfg.merge = function (obj) {
+	readDefaultConfigFiles();
+
 	if (obj instanceof Object) {
-		config = _.merge(config, obj);
+		const env = process.env.NODE_ENV || 'developement';
+		merge(config, obj);
+		merge(config, obj[`$env_${env}`]);
+		return null;
+	}
+
+	return null;
+};
+
+// set values in global config with an object to assign all key values from it
+// if a key already exists then it is assigned with new value
+// if obj is not an Object then nothing happens
+cfg.assign = function (obj) {
+	readDefaultConfigFiles();
+
+	if (obj instanceof Object) {
+		const env = process.env.NODE_ENV || 'developement';
+		Object.assign(config, obj);
+		Object.assign(config, obj[`$env_${env}`]);
 		return null;
 	}
 	return null;
 };
 
 cfg.delete = function (key) {
+	readDefaultConfigFiles();
 	delete config[key];
+};
+
+/**
+ * read config from a file, and merge with existing config
+ * @param {string} file path of the file to read (only absolute paths)
+ * @param {object} options
+ * 	options = {ignoreErrors = ignore all errors, ignoreNotFound = ignore if file not found}
+ */
+cfg.file = function (file, options = {}) {
+	if (!file.startsWith('/')) {
+		throw new Error('Only absolute paths are allowed');
+	}
+
+	if (!configRead) readDefaultConfigFiles();
+
+	try {
+		// eslint-disable-next-line global-require, import/no-dynamic-require
+		const data = require(file);
+		cfg.merge(data);
+	}
+	catch (e) {
+		if (e.code === 'MODULE_NOT_FOUND' && options.ignoreNotFound) {
+			// Ignore Not Found Errors
+			return;
+		}
+		else if (options.ignoreErrors) {
+			// Ignore All Errors
+			return;
+		}
+
+		throw e;
+	}
 };
 
 cfg.is_production = function () {
